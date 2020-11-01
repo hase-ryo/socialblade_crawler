34c90e43e30083720239281ae87bbec0b9bb393b
hase-ryo <26161933+hase-ryo@users.noreply.github.com>
2020-11-01 17:45:00 +0900

    add export

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json
 import re
 import datetime as dt
 import pandas as pd
+import sys
 
 def hichart_js_format(script_text, chart_type):
     # Find specific chart in Hichart script
@@ -32,10 +33,13 @@ def hichart_js_format(script_text, chart_type):
         if line.rstrip() == end_block:
             target_section = False
     if block:
-        format_block = re.sub(r',\s?}', r'}', re.sub(r'\'(.*?)\'', r'"\1"', re.sub(r'(\w+):', r'"\1":', block.lstrip()[len(script_header):-2])))
+        base = block.lstrip()[len(script_header):-2]
+        key_cnv = re.sub(r'(\w+?):', r'"\1":', base)
+        value_cnv = re.sub(r':\s?\'(.*?)\'(,|\s?})', r': "\1"\2', key_cnv)
+        format_block = re.sub(r',\s?}', r'}', re.sub(r'\\\'', '', value_cnv))
         return(json.loads(format_block))
 
-def get_weekly_subscriber(channel, chart_type):
+def get_chart_script(channel, chart_type):
     # Crawling "Detailed Statistics" page about each Youtube channel at SocialBlade.com
     # Find "Highcharts" from HTML, which draws statistic graph
 
@@ -51,21 +55,62 @@ def get_weekly_subscriber(channel, chart_type):
             result = hichart_js_format(script_text, chart_type)
     return(result)
 
-def microsecond_unixtime_to_timestamp(micros):
+def microsecond_unixtime_to_timestamp(micros, mode):
     # SocialBlade.com provide timestamp as unixtime with microseconds
     # And those timezone is PST (-5:00 Hour)
-    # Then, convert timezone PST to JST here
+    # Convert timezone PST to JST here
 
     second_unixtime = dt.datetime.fromtimestamp(micros / 1000)
     timestamp = second_unixtime.replace(tzinfo=dt.timezone(dt.timedelta(hours=-5)))
     jst_timestamp = timestamp.astimezone(dt.timezone(dt.timedelta(hours=9)))
-    return(jst_timestamp)
+    if mode == 'weekly':
+        return(dt.date(jst_timestamp.year, jst_timestamp.month, jst_timestamp.day))
+    elif mode == 'monthly':
+        return(dt.date(jst_timestamp.year, jst_timestamp.month, 1))
+    else:
+        return(jst_timestamp)
+
+def get_target_channels(filepath):
+    # Get channel id from local file
+    # The file include channel id and channel name as JSONNL(line-separated JSON) format
+    # Return result as list of dictionary
+
+    channels = []
+    with open(filepath) as f:
+        lines = f.read().rstrip('\n').split('\n')
+        for line in lines:
+            channels.append(json.loads(line))
+
+    return(channels)
 
 if __name__ == '__main__':
-    channel = 'UCoSrY_IQQVpmIRZ9Xf-y93g'
-    # channel = 'UCAWSyEs_Io8MtpY3m-zqILA'
-    chart_type = 'graph-youtube-daily-weekly-subscribers-container'
-    # chart_type = 'graph-youtube-daily-weekly-vidviews-container'
-    res = get_weekly_subscriber(channel, chart_type)
-    df = pd.DataFrame(list(res['series'][0]['data']), columns=['week', 'subscriber'])
-    print(df)
+    # Get specific chart data from SocialBlade.com
+    # Store each data to DataFrame and merge by timestamp
+    # Export to CSV
+
+    mode = sys.argv[1]
+    channels_filepath = './channels.json'
+    export_filepath = './social_blade_result.csv'
+    if mode == 'weekly':
+        chart_type = 'graph-youtube-daily-weekly-subscribers-container'
+    elif mode == 'monthly':
+        chart_type = 'graph-youtube-monthly-subscribers-container'
+
+    channels = get_target_channels(channels_filepath)
+    result = pd.DataFrame(data=None, index=None, columns=None, dtype=None, copy=False)
+    dfs = []
+    for channel in channels:
+        channel_id = channel['channel_id']
+        name = channel['name']
+        script = get_chart_script(channel_id, chart_type)
+        df = pd.DataFrame(list(script['series'][0]['data']), columns = ['timestamp', name])
+        df['timestamp'] = df['timestamp'].apply(microsecond_unixtime_to_timestamp, mode=mode)
+        dfs.append(df)
+    for d in dfs:
+        if result.empty:
+            result = d
+        else:
+            result = pd.merge(result, d, on='timestamp', how='outer')
+
+    result.to_csv(export_filepath)
+
